f13d0c37d3a8e25c97f1e0d07346d761343329e6
dennisjerdz <dennisjerdz@gmail.com>
2020-01-19 20:43:11 +0800

    home finished

--- ISPRC/Controllers/HomeController.cs
+++ ISPRC/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace ISPRC.Controllers
                 ViewBag.Birds = db.Birds.Count();
                 ViewBag.ActiveRaces = db.Races.Where(x => x.RaceStartDate < now && x.RaceCutOffDate > now).ToList().Count();
                 ViewBag.ActiveRacers = db.BirdsRace.Where(x => x.ArrivalDate != null).ToList().Count();
-                ViewBag.LastRace = db.Races.OrderByDescending(x => x.RaceCutOffDate).FirstOrDefault().RaceName;
+                ViewBag.LastRace = db.Races.OrderByDescending(x => x.RaceCutOffDate).FirstOrDefault()?.RaceName;
             }
 
             if (User.IsInRole("Club Owner"))
@@ -36,13 +36,34 @@ namespace ISPRC.Controllers
                 ViewBag.Races = db.Races.Where(x=>x.ClubId == clubId).ToList().Count();
                 ViewBag.Birds = db.Birds.Include("Owner").Where(x=>x.Owner.ClubId == clubId).Count();
                 ViewBag.ActiveRaces = db.Races.Where(x=>x.ClubId == clubId && x.RaceStartDate < now && x.RaceCutOffDate > now).ToList().Count();
-                ViewBag.ActiveRacers = db.BirdsRace.Include("Races").Where(x=>x.Race.ClubId == clubId && x.ArrivalDate != null).ToList().Count();
-                ViewBag.LastRace = db.Races.OrderByDescending(x => x.RaceCutOffDate).FirstOrDefault(x=>x.ClubId == clubId);
+                ViewBag.ActiveRacers = db.BirdsRace.Include("Race").Where(x=>x.Race.ClubId == clubId && x.ArrivalDate != null).ToList().Count();
+                ViewBag.LastRace = db.Races.OrderByDescending(x => x.RaceCutOffDate).FirstOrDefault(x=>x.ClubId == clubId)?.RaceName;
             }
 
-            if (User.IsInRole("Club Member"))
+            if (User.IsInRole("Member"))
             {
+                string userId = User.Identity.GetUserId();
+                ApplicationUser user = db.Users.FirstOrDefault(u => u.Id == userId);
 
+                ViewBag.Birds = db.Birds.Where(x => x.OwnerId == userId).Count();
+                ViewBag.RacesParticipated = db.BirdsRace.Where(x=>x.Bird.OwnerId == userId).ToList().Count();
+                ViewBag.ActiveRacers = db.BirdsRace.Include("Race").Where(x => x.Bird.OwnerId == userId && x.ArrivalDate != null).ToList().Count();
+                
+                int won = 0;
+                var racesJoined = db.Races.Where(x => x.Birds.Any(y => y.Bird.OwnerId == userId)).ToList();
+                foreach (var r in racesJoined)
+                {
+                    var firstPlace = r.Birds.OrderByDescending(b => b.Speed).FirstOrDefault();
+                    if (firstPlace != null)
+                    {
+                        if (firstPlace.Bird.OwnerId == userId)
+                        {
+                            won++;
+                        }
+                    }
+                }
+
+                ViewBag.RaceWon = won;
             }
 
             return View();
